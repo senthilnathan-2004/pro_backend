d65f8b441e041c8a6ae4f6c05b97cd3d25160a34
senthilnathan-2004 <senthilragunathan2004@gmail.com>
2025-08-30 17:48:15 +0530

    Update usermodel.js

--- model/usermodel.js
+++ model/usermodel.js
@@ -10,6 +10,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+   subject: {
+    type: String,
+    required: true,
+  },
   message: {
     type: String,
     required: true,
