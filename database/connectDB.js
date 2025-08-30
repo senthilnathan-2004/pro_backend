6405ff8f86db836c1a49650df74ebb99b16dc9e2
senthilnathan-2004 <senthilragunathan2004@gmail.com>
2025-08-30 17:32:59 +0530

    Update connectDB.js

--- database/connectDB.js
+++ database/connectDB.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      tls: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("MongoDB Connected");
   } catch (err) {
