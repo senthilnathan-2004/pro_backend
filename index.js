135fe0fcd0c9431c31a18928c45ed2ada912641a
senthilnathan-2004 <senthilragunathan2004@gmail.com>
2025-09-02 23:08:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.set("trust proxy", 1);  
 //cors configuration
 const corsOptions = {
-  origin:["https://senra.run.place/","https://senthilnathan-2004.github.io/","http://localhost:3000"],
+  origin:"*",
   credentials: true,
   optionSuccessStatus: 200,
 };
